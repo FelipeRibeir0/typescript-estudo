f1e0526d35d65b9f901f7f00496630b2b19756b9
FelipeRibeir0 <fecribeiro2003@gmail.com>
2025-08-08 19:52:23 -0300

    estudando tipagem

---
+++ src/tipagemExplicita.ts
@@ -0,0 +1,12 @@
+const myName: string = "Felipe";
+let myAge: number | string = 22;
+let isAdmin: boolean = true
+let namesArray: string[] = ['Felipe','Teste'];
+let address: object;
+
+address = {
+    city: "São Paulo",
+    uf: "SP"
+}
+
+console.log(myName)
